f8e0b217d5f5415e3324bcec8db78658d77a63d7
Michael Ramsey <guitarimp1@aol.com>
2018-07-31 18:52:25 -0700

    First Push

---
+++ Unity Navigation Fundamentals/Assets/Patroller.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patroller : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator anim;
+    public Transform target;
+    Vector3 lastKnownPostion;
+    public Transform eye;
+
+    bool patrolling;
+    public Transform[] patrolTargets;
+    private int destPoint;
+    bool arrived;
+
+	// Use this for initialization
+	void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        lastKnownPostion = transform.position; 
+	}
+	bool CanSeeTarget()
+    {
+        bool canSee = false;
+        Ray ray = new Ray(eye.position, target.transform.position - eye.position);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform != target)
+            {
+                canSee = false;
+            }
+            else
+            {
+                lastKnownPostion = target.transform.position;
+                canSee = true;
+            }
+        }
+        return canSee;
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+		if (agent.pathPending)
+        {
+            return;
+        }
+        if (patrolling)
+        {
+            if (agent.remainingDistance < agent.stoppingDistance)
+            {
+                if (!arrived)
+                {
+                    arrived = true;
+                    StartCoroutine("GoToNextPoint");
+                }
+            }
+            else
+            {
+                arrived = false;
+            }
+        }
+        if (CanSeeTarget())
+        {
+            agent.SetDestination(target.transform.position);
+            patrolling = false;
+            if (agent.remainingDistance < agent.stoppingDistance)
+            {
+                anim.SetBool("Attack", true);
+            }
+            else
+            {
+                anim.SetBool("Attack", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("Attack", false);
+            if (!patrolling)
+            {
+                agent.SetDestination(lastKnownPostion);
+                if (agent.remainingDistance < agent.stoppingDistance)
+                {
+                    patrolling = true;
+                    StartCoroutine("GoToNextPoint");
+                }
+            }
+        }
+        anim.SetFloat("Forward", agent.velocity.sqrMagnitude);
+	}
+    IEnumerator GoToNextPoint()
+    {
+        if (patrolTargets.Length == 0)
+        {
+            yield break;
+        }
+        patrolling = true;
+        yield return new WaitForSeconds(2f);
+        agent.destination = patrolTargets[destPoint].position;
+        destPoint = (destPoint + 1) % patrolTargets.Length;
+    }
+}
---
+++ Unity Navigation Fundamentals/Assets/SmoothFollowing.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollowing : MonoBehaviour {
+    public Vector3 offset;
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+        transform.position = target.position + offset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * 3f);
+	}
+}
